4cec8c7b745c9270c23001aae897f19bbf448af9
Naama <Naama@DESKTOP-HHCI9V7.biu.ac.il>
2023-03-02 16:53:08 +0200

    c5

--- src/primitives/example.java
+++ src/primitives/example.java
@@ -4,7 +4,7 @@ public class example {
 
 	public static void main(String[] args) {
 		int i = 2;
-		System.out.printf("hello world " + i); //dfg
+		System.out.printf("hello world " + i); //dfgg
 
 	}
 
